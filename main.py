98872c4a6d03af00de3e11924e31ba63e0759e9d
hugo20016 <hugo20016@gmail.com>
2022-09-24 14:03:04 +0200

    Create main.py

---
+++ main.py
@@ -0,0 +1,17 @@
+import tkinter as tk
+
+# Create the window
+
+window = tk.Tk()
+
+# Create the label
+
+label = tk.Label(text="Hola, Tkinter")
+
+# Pack the label
+
+label.pack()
+
+# Run the main loop
+
+window.mainloop()
