97762f7ac49b187acb7789c9bde4b31c5363dd87
hugo20016 <hugo20016@gmail.com>
2022-09-24 14:09:04 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,17 +1,1 @@
-import tkinter as tk
-
-# Create the window
-
-window = tk.Tk()
-
-# Create the label
-
-label = tk.Label(text="Hola")
-
-# Pack the label
-
-label.pack()
-
-# Run the main loop
-
-window.mainloop()
+print("Hola mundo")
